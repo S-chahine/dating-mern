e5a9c319b1743e97dcb6c64e08ebb8368b368a34
S-chahine <samah_chahine@outlook.com>
2024-06-12 14:15:38 -0400

    Updated code

--- dating-app-frontend/src/components/DatingCards.js
+++ dating-app-frontend/src/components/DatingCards.js
@@ -8,7 +8,7 @@ const DatingCards = () => {
     const [people, setPeople] = useState([]);
     useEffect(() => {
         async function fetchData() {
-            const req = await axios.get("https://dating-mern.azurewebsites.net/")
+            const req = await axios.get("http://localhost:8001/dating/cards")
             setPeople(req.data)
         }
         fetchData()
